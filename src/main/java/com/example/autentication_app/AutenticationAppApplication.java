02ec982983d2c360acfa89922a484338fae6e1f1
Peter White <petro.bilyi04@gmail.com>
2025-12-10 12:35:44 +0200

    deploy test

--- src/main/java/com/example/autentication_app/AutenticationAppApplication.java
+++ src/main/java/com/example/autentication_app/AutenticationAppApplication.java
@@ -8,7 +8,7 @@ public class AutenticationAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutenticationAppApplication.class, args);
-		System.out.println("DEPLOY TEST");
+		System.out.println("DEPLOY_TEST");
 	}
 
 }
